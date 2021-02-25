9931de3168dee3c6cc09fd4f5a6ba4dd479dc84a
Sergio <sdbeng@gmail.com>
2021-02-25 12:25:05 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+//Challenge: We use what we have learnt in the previous three lessons in order 
+//to visually colour a game board in our browser. At the moment the board is empty. 
+//Please colour the squares, similar to like you would on a chess board using the 
+//for loop.
+
+//Don't forget to share your code in the #share-your-code channel on discord.
+for(let i=0; i<squares.length; i++){
+    // console.log(typeof(squares[i])) //obj
+    // console.log(squares[i]) //div
+    //ask, if the current index is even paint it black
+    //otherwise, leave it as is
+    if(i % 2 === 0){
+        squares[i].style.background = 'black'
+    }
+}
